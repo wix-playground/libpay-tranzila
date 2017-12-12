cd1ccc56cb5c3d5bc6f6e7929efe40614da59756
Raz, Ohad <ohadr@wix.com>
2017-12-12 16:25:39 +0200

    no more spray

--- libpay-tranzila-testkit/src/main/scala/com/wix/pay/tranzila/testkit/TranzilaDriver.scala
+++ libpay-tranzila-testkit/src/main/scala/com/wix/pay/tranzila/testkit/TranzilaDriver.scala
@@ -1,84 +1,85 @@
 package com.wix.pay.tranzila.testkit
 
 
-import java.io.ByteArrayOutputStream
-import java.util.{List => JList}
-
-import com.google.api.client.http.{UrlEncodedContent, UrlEncodedParser}
-import com.wix.hoopoe.http.testkit.EmbeddedHttpProbe
-import spray.http._
-
 import scala.collection.JavaConversions._
 import scala.collection.mutable
+import java.io.ByteArrayOutputStream
+import java.util.{List => JList}
+import akka.http.scaladsl.model.Uri.Path
+import akka.http.scaladsl.model._
+import com.google.api.client.http.{UrlEncodedContent, UrlEncodedParser}
+import com.wix.e2e.http.api.StubWebServer
+import com.wix.e2e.http.client.extractors.HttpMessageExtractors._
+import com.wix.e2e.http.server.WebServerFactory.aStubWebServer
+
 
 class TranzilaDriver(port: Int) {
-  private val probe = new EmbeddedHttpProbe(port, EmbeddedHttpProbe.NotFoundHandler)
+  private val server: StubWebServer = aStubWebServer.onPort(port).build
 
-  def startProbe() {
-    probe.doStart()
-  }
+  def start(): Unit = server.start()
+  def stop(): Unit = server.stop()
+  def reset(): Unit = server.replaceWith()
 
-  def stopProbe() {
-    probe.doStop()
-  }
 
-  def resetProbe() {
-    probe.handlers.clear()
-  }
+  def aRequestFor(params: Map[String, Option[String]]): RequestCtx = new RequestCtx(params)
 
-  def aRequestFor(params: Map[String, Option[String]]): RequestCtx = {
-    new RequestCtx(params)
-  }
 
   class RequestCtx(params: Map[String, Option[String]]) {
     def returns(responseParams: Map[String, String]) {
-      probe.handlers += {
+      server.appendAll {
         case HttpRequest(
         HttpMethods.POST,
-        Uri.Path("/"),
-        headers,
+        Path("/"),
+        _,
         entity,
-        _) if isStubbedRequest(headers, entity) =>
+        _) if isStubbedRequest(entity) =>
           HttpResponse(
             status = StatusCodes.OK,
-            entity = HttpEntity(ContentType(MediaTypes.`application/x-www-form-urlencoded`), urlEncode(responseParams)))
+            entity = HttpEntity(
+              ContentType(MediaTypes.`application/x-www-form-urlencoded`, HttpCharsets.`UTF-8`),
+              urlEncode(responseParams)))
       }
     }
 
     def errors(message: String): Unit = {
       // Yeah, Tranzila returns broken HTML
       val responseHtml =
-        "<html><head><META NAME=\"ROBOTS\" CONTENT=\"NOINDEX, NOFOLLOW\"></head><body>\n<center><h1>Tranzila</h1><br>\nAn error ocurred with the following message:<br>\n<h3><b><font color=red> " +
-        message + " </font></b><br></h3>"
-      probe.handlers += {
+        s"""<html>
+           |  <head>
+           |    <META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
+           |  </head>
+           |  <body>
+           |  <center>
+           |    <h1>Tranzila</h1>
+           |    <br>
+           |    An error ocurred with the following message:<br>
+           |    <h3>
+           |      <b><font color=red>$message</font></b>
+           |      <br>
+           |    </h3>""".stripMargin
+
+      server.appendAll {
         case HttpRequest(
-        HttpMethods.POST,
-        Uri.Path("/"),
-        headers,
-        entity,
-        _) if isStubbedRequest(headers, entity) =>
-          HttpResponse(
-            status = StatusCodes.OK,
-            entity = HttpEntity(ContentType(MediaTypes.`text/html`), responseHtml))
+          HttpMethods.POST,
+          Path("/"),
+          _,
+          entity,
+          _) if isStubbedRequest(entity) =>
+            HttpResponse(
+              status = StatusCodes.OK,
+              entity = HttpEntity(ContentType(MediaTypes.`text/html`, HttpCharsets.`UTF-8`), responseHtml))
       }
     }
 
-    private def isStubbedRequest(headers: List[HttpHeader], entity: HttpEntity): Boolean = {
-      isStubbedHeaders(headers) && isStubbedEntity(entity)
-    }
-
-    private def isStubbedHeaders(headers: List[HttpHeader]): Boolean = {
-      // Tranzila expects this ContentType exactly, e.g. no charset part
-      val contentType = headers.find((_:HttpHeader).name == "Content-Type")
-      contentType.isDefined && contentType.get.value == "application/x-www-form-urlencoded"
-    }
-
-    private def isStubbedEntity(entity: HttpEntity): Boolean = {
-      val requestParams = urlDecode(entity.asString)
-
-      params.forall {
+    private def isStubbedRequest(entity: HttpEntity): Boolean = {
+      val requestParams = urlDecode(entity.extractAsString)
+      val isAllStubbedParams = params.forall {
         case (k, v) => requestParams.contains(k) && v.fold(true)(_ == requestParams(k))
       }
+
+      entity.contentType.mediaType == MediaTypes.`application/x-www-form-urlencoded` &&
+        entity.contentType.charsetOption.isEmpty &&
+        isAllStubbedParams
     }
 
     private def urlEncode(params: Map[String, String]): String = {
--- libpay-tranzila/src/test/scala/com/wix/pay/tranzila/it/TranzilaGatewayIT.scala
+++ libpay-tranzila/src/test/scala/com/wix/pay/tranzila/it/TranzilaGatewayIT.scala
@@ -1,6 +1,10 @@
 package com.wix.pay.tranzila.it
 
 
+import scala.util.Try
+import org.specs2.mutable.SpecWithJUnit
+import org.specs2.specification.Scope
+import com.google.api.client.http.HttpRequestFactory
 import com.google.api.client.http.javanet.NetHttpTransport
 import com.wix.pay.creditcard.{CreditCard, CreditCardOptionalFields, YearMonth}
 import com.wix.pay.model._
@@ -11,65 +15,59 @@ import com.wix.pay.tranzila.model._
 import com.wix.pay.tranzila.testkit.TranzilaDriver
 import com.wix.pay.tranzila.{TranzilaAuthorization, TranzilaMerchant, _}
 import com.wix.pay.{PaymentErrorException, PaymentGateway, PaymentRejectedException}
-import org.specs2.mutable.SpecWithJUnit
-import org.specs2.specification.Scope
 
 
 class TranzilaGatewayIT extends SpecWithJUnit {
   val tranzilaPort = 10009
 
-  val requestFactory = new NetHttpTransport().createRequestFactory()
+  val requestFactory: HttpRequestFactory = new NetHttpTransport().createRequestFactory()
   val driver = new TranzilaDriver(port = tranzilaPort)
+
+  val merchantParser = new JsonTranzilaMerchantParser()
+  val authorizationParser = new JsonTranzilaAuthorizationParser()
+
+  val someMerchant = TranzilaMerchant(username = "some username")
+  val merchantKey: String = merchantParser.stringify(someMerchant)
+
+  val somePayment = Payment(currencyAmount = CurrencyAmount("ILS", 33.3))
+  val someAdditionalFields: CreditCardOptionalFields = CreditCardOptionalFields.withFields(
+    csc = Some("123"),
+    holderId = Some("some holder ID"))
+  val someCreditCard = CreditCard(
+    number = "4012888818888",
+    expiration = YearMonth(2020, 12),
+    additionalFields = Some(someAdditionalFields))
+  val someCustomer = Customer(
+    name = Some(Name(
+      first = "some שם פרטי", // Hebrew characters to test for encoding issues
+      last = "some last name")),
+    email = Some("example@example.org"),
+    phone = Some("+972545000000"))
+
+  val someDeal = Deal(
+    id = "some deal ID",
+    title = Some("some deal title"),
+    description = Some("some deal description"))
+
+  val tranzila: PaymentGateway = new TranzilaGateway(
+    requestFactory = requestFactory,
+    endpointUrl = s"http://localhost:$tranzilaPort/",
+    merchantParser = merchantParser,
+    authorizationParser = authorizationParser)
 
 
   step {
-    driver.startProbe()
+    driver.start()
   }
+
 
   sequential
 
 
   trait Ctx extends Scope {
-    val merchantParser = new JsonTranzilaMerchantParser()
-    val authorizationParser = new JsonTranzilaAuthorizationParser()
-
-    val someMerchant = TranzilaMerchant(
-      username = "some username"
-    )
-    val merchantKey = merchantParser.stringify(someMerchant)
-
-    val somePayment = Payment(currencyAmount = CurrencyAmount("ILS", 33.3))
-    val someAdditionalFields = CreditCardOptionalFields.withFields(
-      csc = Some("123"),
-      holderId = Some("some holder ID"))
-    val someCreditCard = CreditCard(
-      number = "4012888818888",
-      expiration = YearMonth(2020, 12),
-      additionalFields = Some(someAdditionalFields))
-    val someCustomer = Customer(
-      name = Some(Name(
-        first = "some שם פרטי", // Hebrew characters to test for encoding issues
-        last = "some last name"
-      )),
-      email = Some("example@example.org"),
-      phone = Some("+972545000000")
-    )
-
-    val someDeal = Deal(
-      id = "some deal ID",
-      title = Some("some deal title"),
-      description = Some("some deal description")
-    )
-
-    val tranzila: PaymentGateway = new TranzilaGateway(
-      requestFactory = requestFactory,
-      endpointUrl = s"http://localhost:$tranzilaPort/",
-      merchantParser = merchantParser,
-      authorizationParser = authorizationParser
-    )
-
-    driver.resetProbe()
+    driver.reset()
   }
+
 
   "authorize request via Tranzila gateway" should {
     "gracefully fail on invalid merchant key" in new Ctx {
@@ -97,10 +95,7 @@ class TranzilaGatewayIT extends SpecWithJUnit {
         creditCard = someCreditCard,
         payment = somePayment,
         customer = Some(someCustomer),
-        deal = Some(someDeal)
-      ) must beAFailedTry(
-        check = beAnInstanceOf[PaymentErrorException]
-      )
+        deal = Some(someDeal)) must beAFailedTry(check = beAnInstanceOf[PaymentErrorException])
     }
 
     "successfully yield an authorization key on valid request" in new Ctx {
@@ -115,40 +110,33 @@ class TranzilaGatewayIT extends SpecWithJUnit {
         Fields.ccNumber -> Some(someCreditCard.number),
         Fields.expDate -> Some(toTranzilaYearMonth(
           year = someCreditCard.expiration.year,
-          month = someCreditCard.expiration.month
-        )),
+          month = someCreditCard.expiration.month)),
         Fields.cvv -> Some(someCreditCard.csc.get),
         Fields.holderId -> Some(someCreditCard.holderId.get),
         Fields.contact -> Some(someCustomer.name.get.first + " " + someCustomer.name.get.last),
         Fields.email -> Some(someCustomer.email.get),
         Fields.phone -> Some(someCustomer.phone.get),
-        Fields.productDescription -> Some(someDeal.title.get)
-      )) returns Map(
-        Fields.response -> StatusCodes.success,
-        Fields.index -> someIndex,
-        Fields.confirmationCode -> someConfirmationCode
-      )
+        Fields.productDescription -> Some(someDeal.title.get))) returns Map(
+            Fields.response -> StatusCodes.success,
+            Fields.index -> someIndex,
+            Fields.confirmationCode -> someConfirmationCode)
 
-      val authorizationKey = tranzila.authorize(
+      val authorizationKey: Try[String] = tranzila.authorize(
         merchantKey = merchantKey,
         creditCard = someCreditCard,
         payment = somePayment,
         customer = Some(someCustomer),
-        deal = Some(someDeal)
-      )
+        deal = Some(someDeal))
 
       authorizationKey must beASuccessfulTry(
         check = beAuthorizationKey(
           authorization = beAuthorization(
             index = ===(someIndex),
-            confirmationCode = ===(someConfirmationCode)
-          )
-        )
-      )
+            confirmationCode = ===(someConfirmationCode))))
     }
 
     "gracefully fail on rejected card" in new Ctx {
-      val responseCode = StatusCodes.rejected
+      val responseCode: String = StatusCodes.rejected
 
       driver.aRequestFor(Map(
         Fields.supplier -> Some(someMerchant.username),
@@ -171,19 +159,19 @@ class TranzilaGatewayIT extends SpecWithJUnit {
         creditCard = someCreditCard,
         payment = somePayment,
         customer = Some(someCustomer),
-        deal = Some(someDeal)
-      ) must beAFailedTry.like {
-        case e: PaymentRejectedException => e.message must contain(responseCode)
-      }
+        deal = Some(someDeal)) must beAFailedTry.like {
+          case e: PaymentRejectedException => e.message must contain(responseCode)
+        }
     }
   }
+
 
   "capture request via Tranzila gateway" should {
     "successfully yield a transaction ID on valid request" in new Ctx {
       val someAuthorization = TranzilaAuthorization(
         index = "some index",
         confirmationCode = "some confirmation code")
-      val authorizationKey = authorizationParser.stringify(someAuthorization)
+      val authorizationKey: String = authorizationParser.stringify(someAuthorization)
 
       val someAmount = 11.1
       val someIndex = "some returned index"
@@ -200,31 +188,26 @@ class TranzilaGatewayIT extends SpecWithJUnit {
       tranzila.capture(
         merchantKey = merchantKey,
         authorizationKey = authorizationKey,
-        amount = someAmount
-      ) must beASuccessfulTry(
-        check = ===(someIndex)
-      )
+        amount = someAmount) must beASuccessfulTry(check = ===(someIndex))
     }
   }
+
 
   "voidAuthorization request via Tranzila gateway" should {
     "successfully yield a transaction ID on valid request" in new Ctx {
       val someAuthorization = TranzilaAuthorization(
         index = "some index",
         confirmationCode = "some confirmation code")
-      val authorizationKey = authorizationParser.stringify(someAuthorization)
+      val authorizationKey: String = authorizationParser.stringify(someAuthorization)
 
       tranzila.voidAuthorization(
         merchantKey = merchantKey,
-        authorizationKey = authorizationKey
-      ) must beASuccessfulTry(
-        check = ===(someAuthorization.index)
-      )
+        authorizationKey = authorizationKey) must beASuccessfulTry(check = ===(someAuthorization.index))
     }
   }
 
 
   step {
-    driver.stopProbe()
+    driver.stop()
   }
 }
